d7c4ae260d2720ebe7e373134109c1a0e64cb16b
Chandradeep Pokhariya <chinu.deep100@gmail.com>
2021-09-19 14:49:03 +0530

    new updates

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #! /bin/sh
+
 SEQ_NAME=$1
 GENDER=$2
 
 BLEND_PATH="./data/to_annotate/${SEQ_NAME}/annotate/${SEQ_NAME}.blend"
 BLEND_pkl="./data/to_annotate/${SEQ_NAME}/annotate/smplx_param.pkl"
-BLENDER_PATH="~/Desktop/blender/blender"
+BLENDER_PATH="~/Desktop/blender/blender" #if linux
+SYS="macos" # macos
 
 if test -f "$BLEND_pkl"; then
         echo "Continuing from last saved"
         BLEND_PATH="./data/to_annotate/${SEQ_NAME}/annotate/${SEQ_NAME}.blend"
         #linux
-        $BLENDER_PATH $BLEND_PATH
-
+        if [ "$SYS" == "linux" ]; then
+            $BLENDER_PATH $BLEND_PATH
+        elif [ "$SYS" == "macos" ]; then
+            /Applications/blender.app/Contents/MacOS/blender $BLEND_PATH
+        fi
 else
         python prepare_for_annotation.py --seq_name $SEQ_NAME --gender $GENDER
-        $BLENDER_PATH $BLEND_PATH -P starter.py
+        if [ "$SYS" == "linux" ]; then
+            $BLENDER_PATH $BLEND_PATH -P starter.py
+        elif [ "$SYS" == "macos" ]; then
+            /Applications/blender.app/Contents/MacOS/blender $BLEND_PATH -P starter.py
+        fi
+fi
--- starter.py
+++ starter.py
@@ -3,18 +3,23 @@ import sys
 sys.path.append('.')
 from utils import *
 
-if __name__ == '__main__':
-    height, width, num_frame, tcmr_pkl_filepath, ref_video_filepath, gender= read_pkl('./temp.pkl')
+def main():
+    TEMP_FILE = './temp.pkl'
+    PLANE_SCALE_FACTOR = 6
+
+    height, width, num_frame, tcmr_pkl_filepath, ref_video_filepath, gender= read_pkl(TEMP_FILE)
+
+    poses, betas, transs, bboxes, scales, frame_ids = get_param_from_pkl(tcmr_pkl_filepath)
+
     toggle_viewport(gender)
     bpy.data.scenes['Scene'].frame_end = num_frame
 
     ar = width/height
-    factor = 1
+    factor = PLANE_SCALE_FACTOR
 
     add_plane(size =1, scale = (factor, factor/ar, 1))
     add_video_to_plane(ref_video_filepath, num_frame)
 
-    poses, betas, transs, bboxes, scales, frame_ids = get_param_from_pkl(tcmr_pkl_filepath)
     bpy.context.scene.tool_settings.use_keyframe_insert_auto = True
     obj = bpy.data.objects[f'SMPLX-{gender}']
     arm = obj.data
@@ -23,12 +28,6 @@ if __name__ == '__main__':
     bpy.ops.object.mode_set(mode='POSE')
     obj.select_set(True)
 
-    import joblib
-    bboxes = joblib.load('./data/to_annotate/abhi/mpt_results.pkl')[1]['bbox']
-    plane = bpy.data.objects['Plane']
-    pro, all_cx_w, all_cz_w, mesh_height= scale_plane_and_mesh_proportionally(plane, obj, bboxes, width, height)
-
-    masks = glob.glob('./masks/*.png')
     for n in frame_ids:
         idx = list(frame_ids).index(n)
         if idx == 0:
@@ -39,22 +38,12 @@ if __name__ == '__main__':
         pose = poses[idx]
         beta = betas[idx]
         trans = transs[idx]
-        # scale = [scales[0][idx], scales[1][idx]]
         scale = scales[idx]
 
         apply_pose_and_beta(obj, pose, trans, beta)
         bones = bpy.data.objects[f'SMPLX-{gender}'].pose.bones
 
-        # scale_plane_acc_to_mask(masks[idx], bpy.data.objects[f'SMPLX-{gender}'], bpy.data.objects['Plane'])
-        # plane.keyframe_insert(data_path = 'scale', frame = n)
-
         for bone in bones:
-            # if idx == 0:
-            #     if bone.name == 'pelvis':
-            #         bone.scale = (1/scale[0], 1/scale[1], 1)
-            #         # bone.scale = scale
-            #         bone.keyframe_insert(data_path = 'scale', frame = n)
-
             if bone.name == 'root':
                 bone.location = (trans[0], -trans[2], trans[1])
                 bone.keyframe_insert(data_path = 'location', frame = n)
@@ -62,9 +51,8 @@ if __name__ == '__main__':
             bone.keyframe_insert(data_path = 'rotation_quaternion', frame = n)
     bpy.context.scene.tool_settings.use_keyframe_insert_auto = False
 
-# TODO
- # -  run.sh arparase
- # - scale
- # - bbox, fit
- # - Take the bbox height and sort it in the order
- # Take the bounding box and it's center and then place the root there.
+
+
+
+if __name__ == '__main__':
+    main()
